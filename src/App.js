51c77b3dfd9513c217a3b9d17962ebb199744e7a
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-09-25 14:50:50 +0300

    Convert App into class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-        Pokedex App - v1.0.0
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return (
+            <div className="App">
+                Pokedex App - v1.0.0
+            </div>
+        );
+    }
 }
 
 export default App;
